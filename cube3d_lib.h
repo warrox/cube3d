220968419e0c048e8eb3e7beb6e22f88bf17dab5
Warren Hamdi <whamdi@made-f0Ar7s5.clusters.42paris.fr>
2024-09-02 16:15:12 +0200

    created init func + direction func for player init

--- cube3d_lib.h
+++ cube3d_lib.h
@@ -1,13 +1,27 @@
+#include <math.h>
+#ifndef CUBE3D_LIB_h
+#define CUBE3D_LIB_h
+#define PI 3.14159265359
+
+#define g_value 10;
+#define w_value 10;
+
 typedef struct s_player
 {
 	int fov;
+	int distance;
+	int angle;
+	int radians;
 }t_player;
 
 typedef struct s_data
 {
 	t_player player;
+	int wall;
+	int ground;
 }t_data;
 
 
 
 /******Raycasting func**********/
+#endif // !DEBUG
--- src/main.c
+++ src/main.c
@@ -6,14 +6,78 @@
 /*   By: whamdi <whamdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 16:10:46 by whamdi            #+#    #+#             */
-/*   Updated: 2024/08/30 16:11:21 by whamdi           ###   ########.fr       */
+/*   Updated: 2024/09/02 16:14:35 by whamdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+// REVOIR 4. Calcul de la Direction de Chaque Rayon
 #include <stdio.h>
-int main(int argc, char **argv)
+#include "../cube3d_lib.h"
+#define MAP_WIDTH 6
+#define MAP_HEIGHT 5
+
+void init_player(t_data *data)
 {
-	printf("Welcome to cube3d\n");
+	data->player.fov = 0;
+	data->player.angle = 0;
+	data->player.distance = 0;
+	data->wall = 0;
+	data->ground = 0;
+} 
+double get_angle_posplayer(char player_dir)
+{
+	if(player_dir == 'E')
+	{
+		return(0);
+	}
+	if(player_dir == 'N')
+	{
+		return(PI/2);
+	}
+	if(player_dir == 'S')
+	{
+		return(3 * PI / 2);
+	}
+	if(player_dir == 'W')
+	{
+		return(PI);
+	}
 	return(0);
+}
+char map[MAP_HEIGHT][MAP_WIDTH + 1] = {
+    "111111",
+    "100001",
+    "10N001",
+    "100001",
+    "111111"
+};
+
+int main() {
+    t_data data;
+	// Afficher la carte pour vérification
+    for (int i = 0; i < MAP_HEIGHT; i++) {
+        printf("%s\n", map[i]);
+    }
+	init_player(&data); 
+    // Initialisation du joueur
+    int player_x = 0, player_y = 0;
+    char player_dir = ' ';
+
+    // Rechercher la position initiale du joueur
+    for (int i = 0; i < MAP_HEIGHT; i++) {
+        for (int j = 0; j < MAP_WIDTH; j++) {
+            if (map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'W' || map[i][j] == 'E') {
+                player_x = j;
+                player_y = i;
+				get_angle_posplayer(map[i][j]);
+                player_dir = map[i][j];
+                map[i][j] = '0'; // Remplace la position par un espace vide une fois que tu as enregistré la position du joueur
+                break;
+            }
+        }
+    }
+
+    printf("Player start position: (%d, %d)\n", player_x, player_y);
+    printf("Player start direction: %c\n", player_dir);
+    return 0;
 }
