fcb2ff75af75ab7fcfef4b84bedfa05fcf4486fc
Cyprien Ferreira <cyferrei@paul-f4Br5s7.clusters.42paris.fr>
2024-09-05 11:59:53 +0200

    [ADD]: add new errors functions

--- src/parsing/error_handler.c
+++ src/parsing/error_handler.c
@@ -6,11 +6,26 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 09:23:53 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/05 09:49:36 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/09/05 10:45:57 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D_lib.h"
+
+void	error_split(t_data *data)
+{
+	free(data->map->map_line_cpy);
+	free(data->map);
+	error_msg("Fail to allocate memory for map_tab!");
+}
+
+void	error_cpy(t_data *data, char *map_line)
+{
+	free(map_line);
+	close(data->map->fd);
+	free(data->map);
+	error_msg("Fail to copy map!");
+}
 
 void	error_open(t_data *data, char *map_line)
 {
