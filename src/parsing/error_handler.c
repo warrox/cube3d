f9bf717e887e8ff39be82936cd07041fa18d0dde
Cyprien Ferreira <cyferrei@paul-f4Br5s7.clusters.42paris.fr>
2024-09-05 18:26:51 +0200

    [ADD]: add new functions in case of error - WARNING: need to create new ones for part implemented

--- src/parsing/error_handler.c
+++ src/parsing/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 09:23:53 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/05 10:45:57 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/09/05 15:09:21 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,30 @@
 
 void	error_split(t_data *data)
 {
-	free(data->map->map_line_cpy);
-	free(data->map);
-	error_msg("Fail to allocate memory for map_tab!");
+	free(data->file->map_line_cpy);
+	free(data->file);
+	error_msg("Fail to allocate memory for tab_map!");
 }
 
 void	error_cpy(t_data *data, char *map_line)
 {
 	free(map_line);
-	close(data->map->fd);
-	free(data->map);
+	close(data->file->fd);
+	free(data->file);
 	error_msg("Fail to copy map!");
 }
 
 void	error_open(t_data *data, char *map_line)
 {
 	free(map_line);
-	free(data->map);
+	free(data->file);
 	error_msg("Fail to open file!");
 }
 
 void	error_read(t_data *data, char *map_line, char *msg)
 {
 	free(map_line);
-	close(data->map->fd);
+	close(data->file->fd);
 	error_msg(msg);
 }
 
--- src/parsing/error_handler_one.c
+++ src/parsing/error_handler_one.c
@@ -6,16 +6,24 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 11:24:01 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/05 11:32:24 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/09/05 15:09:21 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D_lib.h"
 
+void	error_split_sett(t_data *data)
+{
+	free_split(data->file->tab_data);
+	free(data->file->map_line_cpy);
+	free(data->file);
+	error_msg("Fail to allocate memory for split_settings!");
+}
+
 void	error_order(t_data *data)
 {
-	free_split(data);
-	free(data->map->map_line_cpy);
-	free(data->map);
+	free_split(data->file->tab_data);
+	free(data->file->map_line_cpy);
+	free(data->file);
 	error_msg("Wrong order!");
 }
