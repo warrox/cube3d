c0ff0d9c7ebd527b1272b543fa9fc7d20de55c56
Cyprien Ferreira <cyferrei@paul-f4Br5s7.clusters.42paris.fr>
2024-09-05 18:27:22 +0200

    [ADD]: add new file for free functions

--- src/parsing/free_handler.c
+++ src/parsing/free_handler.c
@@ -6,28 +6,28 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 08:38:59 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/05 10:52:27 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/09/05 17:37:25 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D_lib.h"
 
-void	free_split(t_data *data)
+void	free_split(char **split)
 {
 	int	i;
 
 	i = ZERO_INIT;
-	while(data->map->data_tab[i])
+	while(split[i])
 	{
-		free(data->map->data_tab[i]);
+		free(split[i]);
 		i++;
 	}
-	free(data->map->data_tab);
+	free(split);
 }
 
-void	free_map_struct(t_data *data)
+void	free_file_struct(t_data *data)
 {
-	free_split(data);
-	free(data->map->map_line_cpy);
-	free(data->map);
+	free_split(data->file->tab_data);
+	free(data->file->map_line_cpy);
+	free(data->file);
 }
