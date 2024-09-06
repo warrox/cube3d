39df7d0e5608d46594ef407039e2bc8a9740abb6
cyferrei <ferreiracyprien@gmail.com>
2024-09-06 19:42:32 +0200

    [MODIF]: modif error handling - in help with file.png in this repos

--- src/parsing/error_handler.c
+++ src/parsing/error_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_handler.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cyprien <cyprien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 09:23:53 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/05 15:09:21 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/09/06 19:13:16 by cyprien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,33 @@
 
 void	error_split(t_data *data)
 {
+	free(data->color);
+	free(data->map);
 	free(data->file->map_line_cpy);
-	free(data->file);
-	error_msg("Fail to allocate memory for tab_map!");
+	error_alloc_mapline(data, "Fail to allocate memory for tab_map!");
 }
 
-void	error_cpy(t_data *data, char *map_line)
+void	error_read(t_data *data, char *msg)
 {
-	free(map_line);
 	close(data->file->fd);
-	free(data->file);
-	error_msg("Fail to copy map!");
+	error_open(data, msg);
 }
 
-void	error_open(t_data *data, char *map_line)
+void	error_open(t_data *data, char *msg)
 {
-	free(map_line);
-	free(data->file);
-	error_msg("Fail to open file!");
+	free(data->color);
+	free(data->map);
+	free(data->file->map_line);
+	error_alloc_mapline(data, msg);
 }
 
-void	error_read(t_data *data, char *map_line, char *msg)
+void	error_alloc_mapline(t_data *data, char *msg)
 {
-	free(map_line);
-	close(data->file->fd);
-	error_msg(msg);
+	free(data->file);
+	error_alloc_file(msg);
 }
 
-void	error_msg(char *msg)
+void	error_alloc_file(char *msg)
 {
 	printf("\033[31mError\n%s\n\033[0m", msg);
 	exit(EXIT_FAILURE);
