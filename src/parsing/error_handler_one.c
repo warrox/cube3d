98bac7197b7de9e1d4156f5c804714c6606e9b39
Cyprien Ferreira <cyferrei@paul-f4Br4s8.clusters.42paris.fr>
2024-09-09 16:54:50 +0200

    [MODIF]: modification on error_order

--- src/parsing/error_handler_one.c
+++ src/parsing/error_handler_one.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_handler_one.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cyprien <cyprien@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 11:24:01 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/06 19:34:16 by cyprien          ###   ########.fr       */
+/*   Updated: 2024/09/09 14:57:05 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,11 @@ void	error_split_value(t_data *data, char *msg)
 	error_alloc_mapline(data, msg);
 }
 
-void	error_order(t_data *data)
+void	error_order(t_data *data, char *msg)
 {
 	free(data->color);
 	free(data->map);
 	free_split(data->file->tab_data);
 	free(data->file->map_line_cpy);
-	error_alloc_mapline(data, "Wrong order!");
+	error_alloc_mapline(data, msg);
 }
