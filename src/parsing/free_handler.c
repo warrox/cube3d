b71815b9163446405de2018824c1dccf88e61f59
cyferrei <ferreiracyprien@gmail.com>
2024-09-11 15:46:40 +0200

    [ADD]: add free function to free all duplicate data

--- src/parsing/free_handler.c
+++ src/parsing/free_handler.c
@@ -6,11 +6,31 @@
 /*   By: cyprien <cyprien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 08:38:59 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/11 01:34:41 by cyprien          ###   ########.fr       */
+/*   Updated: 2024/09/11 15:45:08 by cyprien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D_lib.h"
+
+void	free_duplicate_data(t_data *data)
+{
+	int	i;
+
+	i = ZERO_INIT;
+	while(i < data->file->line_data)
+	{
+		free(data->file->infos[i]);
+		i++;
+	}
+	free(data->file->infos);
+	i = 0;
+	while(i < data->file->line_map)
+	{
+		free(data->file->map[i]);
+		i++;
+	}
+	free(data->file->map);
+}
 
 void	free_split(char **split)
 {
@@ -31,5 +51,6 @@ void	free_file_struct(t_data *data)
 	free_split(data->file->tab_data);
 	free(data->file->color);
 	free(data->file->path);
+	free_duplicate_data(data);
 	free(data->file);
 }
