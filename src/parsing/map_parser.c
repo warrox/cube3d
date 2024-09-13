bb2641f76d939cca7e861d0f0732d3223a1eb710
Cyprien Ferreira <cyferrei@paul-f4Br7s1.clusters.42paris.fr>
2024-09-13 14:38:46 +0200

    [ADD]: checker invalid char added - error free leaks if troubles

--- src/parsing/map_parser.c
+++ src/parsing/map_parser.c
@@ -6,11 +6,12 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 16:43:09 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/09/12 17:39:36 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/09/13 14:35:34 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D_lib.h"
+#include <stdlib.h>
 
 int	check_char(t_data *data)
 {
@@ -23,15 +24,14 @@ int	check_char(t_data *data)
 	tmp = data->file->map;
 	while(i < data->file->line_map)
 	{
+		j = 0;
 		while(tmp[i][j])
 		{
 			if (tmp[i][j] != ' ' && tmp[i][j] != '\t' && tmp[i][j] != '1' && 
 				tmp[i][j] != '0' && tmp[i][j] != 'N' && tmp[i][j] != 'S' && 
 				tmp[i][j] != 'E' && tmp[i][j] != 'W')
-				{
-					dprintf(2, "Invalid char: [%d] at i: %d j: %d\n", tmp[i][j], i, j);
 					return (0);
-				}
+
 			j++;
 		}
 		i++;
@@ -42,9 +42,5 @@ int	check_char(t_data *data)
 void	map_parser(t_data *data)
 {
 	if(!check_char(data))
-	{
-		dprintf(2, "INVALID CHAR IN MAP!\n");
-		exit(1);
-		// error;
-	}
+		error_map(data, "Invalid char detected int map!");
 }
