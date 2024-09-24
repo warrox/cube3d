0f452849612cfba926664c60927a5d9936fa3372
Warren Hamdi <whamdi@paul-f5Ar2s4.clusters.42paris.fr>
2024-09-24 09:47:41 +0200

    Update main

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: whamdi <whamdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 16:10:46 by whamdi            #+#    #+#             */
-/*   Updated: 2024/09/20 14:12:49 by whamdi           ###   ########.fr       */
+/*   Updated: 2024/09/20 15:07:37 by whamdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D_lib.h"
+#include <stdio.h>
 
 // implement a calculation function to build the fov
 // int cpy_map(t_data *data)
@@ -37,6 +38,19 @@
 // 	return(0);
 // }
 
+unsigned int rgb_to_hex(int r, int g, int b) {
+    // S'assurer que les valeurs r, g, b sont entre 0 et 255
+    if (r < 0) r = 0;
+    if (r > 255) r = 255;
+    if (g < 0) g = 0;
+    if (g > 255) g = 255;
+    if (b < 0) b = 0;
+    if (b > 255) b = 255;
+
+    // Combiner les valeurs r, g, b en une valeur hexadécimale
+    return (r << 16) | (g << 8) | b;
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
@@ -51,6 +65,8 @@ int	main(int argc, char **argv, char **envp)
 	map_parser(&data);
 	print_final_datas(&data);
 	init_player(&data);
+	int test = rgb_to_hex(data.file->color->c_r,data.file->color->c_g, data.file->color->c_b);
+	printf("test : %d\n",test);
 	// cpy_map(&data);
 	for (int i = 0; i < data.file->line_map; i++)
 	{
@@ -68,6 +84,7 @@ int	main(int argc, char **argv, char **envp)
 		}
 	}
 	printf("player x %f\t player y %f\n ", data.player.x, data.player.y);
+	
 	init_mlx(&data);
 	mlx_hook(data.mlx.mlx_win, KeyPress, KeyPressMask, key_handler, &data);
 	mlx_hook(data.mlx.mlx_win, 17,0,close_window, &data);
