26e2544f51f59e9e6be1bba51927c2902c553588
Warren Hamdi <whamdi@made-f0Br3s11.clusters.42paris.fr>
2024-10-05 10:56:53 +0200

    [raycast.c] : norm

--- src/raycast/raycast.c
+++ src/raycast/raycast.c
@@ -1,23 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycast.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: whamdi <whamdi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 10:54:56 by whamdi            #+#    #+#             */
+/*   Updated: 2024/10/05 10:55:14 by whamdi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/cub3D_lib.h"
+
 int	minimap_render(void *param)
 {
-
 	t_data	*data;
 
-	data = (t_data *)param;	
-	// printf("string : %s\n", data->map_test[0]);
+	data = (t_data *)param;
 	data->cell_width = (WIDTH / 6) / data->file->max_len;
 	data->cell_height = (HEIGHT / 6) / data->file->line_map;
 	mlx_destroy_image(data->mlx.p_mlx, data->mlx.img);
 	data->mlx.img = mlx_new_image(data->mlx.p_mlx, WIDTH, HEIGHT);
 	data->mlx.addr = mlx_get_data_addr(data->mlx.img, &data->mlx.bits_per_pixel,
 			&data->mlx.line_length, &data->mlx.endian);
-	
-	// Dessiner le joueur après la mini-map
-	// faire nouvel fonction a partir d'ici
 	mlx_put_image_to_window(data->mlx.p_mlx, data->mlx.mlx_win, data->mlx.img,
 		0, 0);
 	update_player_pos(data, data->player.x, data->player.y);
 	return (0);
 }
-
